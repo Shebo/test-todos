bd3b8c0ce03b1732a15f98e28404bfc78c808dae
Shebo <shebo6@gmail.com>
2016-12-29 01:42:30 +0200

    Add GET /api/todos/:id

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
-var {mongoose} = require('./db/mongoose');
+var {mongoose, mongoose: {Types: {ObjectId}}} = require('./db/mongoose');
 var {Todo} = require('./models/Todo');
 var {User} = require('./models/User');
 
@@ -9,11 +9,32 @@ var app = express();
 
 app.use(bodyParser.json());
 
+// get all todos
 app.get('/api/todos', function(req, res){
     Todo.find().then(function(todos){
         res.send({todos: todos});
     }).catch(function(err){
         res.status(400).send(err);
+    });
+});
+
+// get a todo by it's ID
+app.get('/api/todos/:id', function(req, res){
+
+    return new Promise(function(resolve, reject){
+        if(ObjectId.isValid(req.params.id)){
+            resolve();
+        }else{
+            reject('ID is not valid.');
+        }
+    }).then(function(){
+        return Todo.findById(req.params.id);
+    }).then(function(todo){
+        return (todo) ? Promise.resolve(todo) : Promise.reject('ID is not found.');
+    }).then(function(todo){
+        res.send({todo: todo});
+    }).catch(function(err){
+        res.status(404).send(err);
     });
 });
 
