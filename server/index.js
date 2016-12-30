3d036b54799276e286db2f317590ff706369326a
Shebo <shebo6@gmail.com>
2016-12-30 16:00:18 +0200

    Setup env variables

--- server/index.js
+++ server/index.js
@@ -2,11 +2,13 @@ const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
 
+require('../config');
+
 var {mongoose, mongoose: {Types: {ObjectId}}} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 var app = express();
 
 app.use(bodyParser.json());
